ae8823e6b3bf00ca8efbbe05a7dd152b9f0f853f
Broccoli Wither <mathewmartin722@gmail.com>
2025-02-15 22:18:35 -0800

    fix textures

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  publicDir: 'public', // Change public directory to 'static'
-  // publicDir: path.resolve(__dirname, 'assets'), // Use an absolute path
-  // publicDir: false, // Disable the public directory
+  base: process.env.NODE_ENV === 'production' ? '/Dead-Desert/' : '/',
+  publicDir: 'public',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
 })
