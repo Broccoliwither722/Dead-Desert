3d60b2446df15a1bb0ec9607ce38df4fc36b7092
Broccoli Wither <mathewmartin722@gmail.com>
2025-02-16 14:21:48 -0800

    armored zombies and cactus texture

--- src/actors/ammoBox.ts
+++ src/actors/ammoBox.ts
@@ -1,15 +1,26 @@
 import * as ex from 'excalibur'
 import { Player } from './player'
+import { Resources } from '../resources'
 
 export class AmmoBox extends ex.Actor {
   constructor(pos: ex.Vector) {
     super({
       pos,
-      width: 12,
-      height: 36,
+      width: 36,
+      height: 25,
       color: ex.Color.fromHex('#1a472a'), // Dark green
       collisionType: ex.CollisionType.Passive,
     })
+    this.graphics.use(
+      Resources.AmmoBox.toSprite({
+        destSize: {
+          // Optionally specify a different projected size, otherwise use the source
+          width: 36,
+          height: 36
+          ,
+        },
+      })
+    )
   }
 
   onInitialize() {
